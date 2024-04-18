5737f57a49c8ad94ba8d43c2512486934b7209b2
Somto Ufondu <somtoufondu@gmail.com>
2024-04-18 05:47:29 -0600

    Added an array of movements and experimented with setTimeout and setInterval methods to cycle through the movements of the snake.

--- client.js
+++ client.js
@@ -10,9 +10,26 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-  conn.on('connect', () => {
-    console.log('Successfully connected to game server!');
-    conn.write('Name: SU');
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server!");
+
+    conn.write("Name: SU");
+    
+    const movements = ["Move: up", "Move: up", "Move: left", "Move: left","Move: down", "Move: down", "Move: right", "Move: right"];
+    let delay = 1000;
+    let movementsIndex = 0;
+
+    // for (let movement of movements) {
+    //   setTimeout(() => {
+    //     conn.write(movement);
+    //   }, delay);
+    //   delay += 1000;
+    // };
+
+    setInterval(() => {
+      conn.write(movements[movementsIndex]);
+      movementsIndex = (movementsIndex + Math.floor(Math.random() * 10)) % movements.length;
+    }, delay);
   });
 
   conn.on("data", (data) => {
