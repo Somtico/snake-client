7c311b8662ddcacfbb0ed19b01735e7755ca1068
Somto Ufondu <somtoufondu@gmail.com>
2024-04-18 15:53:32 -0600

    add code to send data from the input module to the server and send movement commands.

--- client.js
+++ client.js
@@ -15,21 +15,21 @@ const connect = function () {
 
     conn.write("Name: SU");
     
-    const movements = ["Move: up", "Move: up", "Move: left", "Move: left","Move: down", "Move: down", "Move: right", "Move: right"];
-    let delay = 1000;
-    let movementsIndex = 0;
+    // const movements = ["Move: up", "Move: up", "Move: left", "Move: left","Move: down", "Move: down", "Move: right", "Move: right"];
+    // let delay = 1000;
+    // let movementsIndex = 0;
 
-    // for (let movement of movements) {
-    //   setTimeout(() => {
-    //     conn.write(movement);
-    //   }, delay);
-    //   delay += 1000;
-    // };
+    // // for (let movement of movements) {
+    // //   setTimeout(() => {
+    // //     conn.write(movement);
+    // //   }, delay);
+    // //   delay += 1000;
+    // // };
 
-    setInterval(() => {
-      conn.write(movements[movementsIndex]);
-      movementsIndex = (movementsIndex + Math.floor(Math.random() * 10)) % movements.length;
-    }, delay);
+    // setInterval(() => {
+    //   conn.write(movements[movementsIndex]);
+    //   movementsIndex = (movementsIndex + Math.floor(Math.random() * 10)) % movements.length;
+    // }, delay);
   });
 
   conn.on("data", (data) => {
--- input.js
+++ input.js
@@ -1,13 +1,33 @@
+let connection;
+
 const handleUserInput = function (key) {
   // \u0003 maps to ctrl+c input
   if (key === "\u0003") {
     process.exit();
   }
+
+  if (key === 'w' || key === "\u001b[A") {
+    connection.write('Move: up');
+  }
+
+  if (key === 'a' || key === "\u001b[D") {
+    connection.write('Move: left');
+  }
+
+  if (key === 's' || key === "\u001b[B") {
+    connection.write('Move: down');
+  }
+
+  if (key === 'd' || key === "\u001b[C") {
+    connection.write('Move: right');
+  }
 };
 
 // setup interface to handle user input from stdin
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
